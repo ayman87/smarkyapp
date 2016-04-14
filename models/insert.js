80447abf95a55733cfc71849bed2556907f5c9c9
Mohamed Ayman <m.ayman854@gmail.com>
2016-04-14 12:17:16 +0200

    removed a connecton in users.js

--- models/insert.js
+++ models/insert.js
@@ -1,23 +1,23 @@
 var pg = require('pg');
-var connectionString = process.env.DATABASE_URL || "postgres://gibhvqdyyuudcx:ZEloXSWrMEUpuUCa3e1QKb8IrC@ec2-23-21-42-29.compute-1.amazonaws.com:5432/dfr3e9sb0p3mu";
+var connectionString = process.env.DATABASE_URL || "postgres://localhost:5432/nfcbookdb";
 
 var client = new pg.Client(connectionString);
 client.connect();
 
 
-var queryinsert= client.query("INSERT INTO BOOKS VALUES(default,1111,7,clock_timestamp(),'Serawy')");
+var queryinsert= client.query("INSERT INTO BOOKS VALUES(default,'1111','7',clock_timestamp(),'Serawy')");
 
-var queryinsert1= client.query("INSERT INTO RFIDS VALUES(default,1,7,1,1111,clock_timestamp(),clock_timestamp())");
+var queryinsert1= client.query("INSERT INTO RFIDS VALUES(default,1,'7',1,'1111',clock_timestamp(),clock_timestamp())");
 
-var queryinsert2= client.query("INSERT INTO admin_answers VALUES(default,1111,1,1,7,clock_timestamp(),clock_timestamp(),'Hello dido?','please')");
+var queryinsert2= client.query("INSERT INTO admin_answers VALUES(default,'1111',1,1,'7',clock_timestamp(),clock_timestamp(),'Hello dido?','please')");
 
-var queryinsert3= client.query("INSERT INTO QUESTIONS VALUES(default,1,1111,1,7,clock_timestamp(),clock_timestamp(),'hello?',5,'Alan','2.5')");
+var queryinsert3= client.query("INSERT INTO QUESTIONS VALUES(default,1,'1111',1,'7',clock_timestamp(),clock_timestamp(),'hello?',5,'Alan','2.5')");
 
-var queryinsert4= client.query("INSERT INTO ANSWERS VALUES(default,1,1,1111,1,7,clock_timestamp(),clock_timestamp(),'yea',5,'Alan','2.5')");
+var queryinsert4= client.query("INSERT INTO ANSWERS VALUES(default,1,1,'1111',1,'7',clock_timestamp(),clock_timestamp(),'yea',5,'Alan','2.5')");
 
-var queryinsert5= client.query("INSERT INTO LINKS VALUES(default,1,1111,1,7,clock_timestamp(),clock_timestamp(),'www.google.com',5,'Alan','2.5')");
+var queryinsert5= client.query("INSERT INTO LINKS VALUES(default,1,'1111',1,'7',clock_timestamp(),clock_timestamp(),'www.google.com',5,'Alan','2.5')");
 
-var queryinsert6= client.query("INSERT INTO SUMMARIES VALUES(default,1,1111,1,7,clock_timestamp(),clock_timestamp(),'Doing a good job',5,'Alan','2.5')");
+var queryinsert6= client.query("INSERT INTO SUMMARIES VALUES(default,1,'1111',1,'7',clock_timestamp(),clock_timestamp(),'Doing a good job',5,'Alan','2.5')");
 
 var queryinsert7= client.query("INSERT INTO USERS VALUES(default,'Alan Harber','12345','Alan@gmail.com')");
 
--- models/nfcbookdb.js
+++ models/nfcbookdb.js
@@ -1,5 +1,5 @@
 var pg = require('pg');
-var connectionString = process.env.DATABASE_URL || "postgres://gibhvqdyyuudcx:ZEloXSWrMEUpuUCa3e1QKb8IrC@ec2-23-21-42-29.compute-1.amazonaws.com:5432/dfr3e9sb0p3mu";
+var connectionString = process.env.DATABASE_URL || "postgres://localhost:5432/nfcbookdb";
 var client = new pg.Client(connectionString);
 client.connect();
 
@@ -7,23 +7,23 @@ client.connect();
 
 var querytz = client.query('Alter DATABASE nfcbookdb SET TIME ZONE "Africa/Cairo"');
 
-var query = client.query('CREATE TABLE BOOKS(id SERIAL unique ,ISBN INT , EDITION INT , UpdatedAt timestamp , NAME VARCHAR(100),PRIMARY KEY(ISBN,EDITION) )');
+var query = client.query('CREATE TABLE BOOKS(id SERIAL unique ,ISBN VARCHAR(20) , EDITION VARCHAR(20) , UpdatedAt timestamp , NAME VARCHAR(100),PRIMARY KEY(ISBN,EDITION) )');
 
-var query1 = client.query('CREATE TABLE RFIDS(id SERIAL,bid INT ,EDITION INT ,PageNumber INT, ISBN INT,updatedAt timestamp , createdAt timestamp ,PRIMARY KEY(id,ISBN,EDITION) ,FOREIGN KEY (bid) REFERENCES BOOKS (id), unique(PageNumber,bid))');
+var query1 = client.query('CREATE TABLE RFIDS(id SERIAL,bid INT ,EDITION VARCHAR(20) ,PageNumber INT, ISBN VARCHAR(20),updatedAt timestamp , createdAt timestamp ,PRIMARY KEY(id,ISBN,EDITION) ,FOREIGN KEY (bid) REFERENCES BOOKS (id), unique(PageNumber,bid))');
 
-var query2 = client.query('CREATE TABLE admin_answers(id SERIAL,ISBN INT,bid INT , PageNumber INT , EDITION INT, updatedAt timestamp, createdAt timestamp,QUESTION text, ANSWER text , PRIMARY KEY(id,ISBN,EDITION,PageNumber), FOREIGN KEY(ISBN,EDITION) REFERENCES BOOKS(ISBN,EDITION),FOREIGN KEY(PageNumber,bid) REFERENCES RFIDS(PageNumber,bid))');
+var query2 = client.query('CREATE TABLE admin_answers(id SERIAL,ISBN VARCHAR(20),bid INT , PageNumber INT , EDITION VARCHAR(20), updatedAt timestamp, createdAt timestamp,QUESTION text, ANSWER text , PRIMARY KEY(id,ISBN,EDITION,PageNumber), FOREIGN KEY(ISBN,EDITION) REFERENCES BOOKS(ISBN,EDITION),FOREIGN KEY(PageNumber,bid) REFERENCES RFIDS(PageNumber,bid))');
 
-var query3 = client.query('CREATE TABLE QUESTIONS(id SERIAL unique,bid INT,ISBN INT , PageNumber INT , EDITION INT, updatedAt timestamp,createdAt timestamp, QUESTION text,RatingCount INT, USERID VARCHAR(40) ,RATING VARCHAR(20),  PRIMARY KEY(id,ISBN,EDITION,PageNumber), FOREIGN KEY(ISBN,EDITION) REFERENCES BOOKS(ISBN,EDITION) ,FOREIGN KEY(PageNumber,bid) REFERENCES RFIDS(PageNumber,bid))');
-
-
-var query4 = client.query('CREATE TABLE ANSWERS(id SERIAL, bid INT,qid INT ,ISBN INT , PageNumber INT , EDITION INT, updatedAt timestamp,createdAt timestamp,ANSWER text,RatingCount INT, USERID VARCHAR(40) ,RATING VARCHAR(20), PRIMARY KEY(id,ISBN,EDITION,PageNumber), FOREIGN KEY(ISBN,EDITION) REFERENCES BOOKS(ISBN,EDITION),FOREIGN KEY(qid) REFERENCES QUESTIONS(id), FOREIGN KEY(PageNumber,bid) REFERENCES RFIDS(PageNumber,bid) )');
+var query3 = client.query('CREATE TABLE QUESTIONS(id SERIAL unique,bid INT,ISBN VARCHAR(20) , PageNumber INT , EDITION VARCHAR(20), updatedAt timestamp,createdAt timestamp, QUESTION text,RatingCount INT, USERID VARCHAR(40) ,RATING VARCHAR(20),  PRIMARY KEY(id,ISBN,EDITION,PageNumber), FOREIGN KEY(ISBN,EDITION) REFERENCES BOOKS(ISBN,EDITION) ,FOREIGN KEY(PageNumber,bid) REFERENCES RFIDS(PageNumber,bid))');
 
 
-var query5 = client.query('CREATE TABLE LINKS(id SERIAL,bid INT,ISBN INT , PageNumber INT , EDITION INT, updatedAt timestamp, createdAt timestamp,LINK text,RatingCount INT, USERID VARCHAR(40) ,RATING VARCHAR(20), PRIMARY KEY(id,ISBN,EDITION,PageNumber), FOREIGN KEY(ISBN,EDITION) REFERENCES BOOKS(ISBN,EDITION),FOREIGN KEY(PageNumber,bid) REFERENCES RFIDS(PageNumber,bid))');
+var query4 = client.query('CREATE TABLE ANSWERS(id SERIAL, bid INT,qid INT ,ISBN VARCHAR(20) , PageNumber INT , EDITION VARCHAR(20), updatedAt timestamp,createdAt timestamp,ANSWER text,RatingCount INT, USERID VARCHAR(40) ,RATING VARCHAR(20), PRIMARY KEY(id,ISBN,EDITION,PageNumber), FOREIGN KEY(ISBN,EDITION) REFERENCES BOOKS(ISBN,EDITION),FOREIGN KEY(qid) REFERENCES QUESTIONS(id), FOREIGN KEY(PageNumber,bid) REFERENCES RFIDS(PageNumber,bid) )');
+
+
+var query5 = client.query('CREATE TABLE LINKS(id SERIAL,bid INT,ISBN VARCHAR(20) , PageNumber INT , EDITION VARCHAR(20), updatedAt timestamp, createdAt timestamp,LINK text,RatingCount INT, USERID VARCHAR(40) ,RATING VARCHAR(20), PRIMARY KEY(id,ISBN,EDITION,PageNumber), FOREIGN KEY(ISBN,EDITION) REFERENCES BOOKS(ISBN,EDITION),FOREIGN KEY(PageNumber,bid) REFERENCES RFIDS(PageNumber,bid))');
 
 
 
-var query6 = client.query('CREATE TABLE SUMMARIES(id SERIAL,bid INT,ISBN INT , PageNumber INT , EDITION INT, updatedAt timestamp, createdAt timestamp,SUMMARY text,RatingCount INT, USERID VARCHAR(40) ,RATING VARCHAR(20), PRIMARY KEY(id,ISBN,EDITION,PageNumber), FOREIGN KEY(ISBN,EDITION) REFERENCES BOOKS(ISBN,EDITION),FOREIGN KEY(PageNumber,bid) REFERENCES RFIDS(PageNumber,bid))');
+var query6 = client.query('CREATE TABLE SUMMARIES(id SERIAL,bid INT,ISBN VARCHAR(20) , PageNumber INT , EDITION VARCHAR(20), updatedAt timestamp, createdAt timestamp,SUMMARY text,RatingCount INT, USERID VARCHAR(40) ,RATING VARCHAR(20), PRIMARY KEY(id,ISBN,EDITION,PageNumber), FOREIGN KEY(ISBN,EDITION) REFERENCES BOOKS(ISBN,EDITION),FOREIGN KEY(PageNumber,bid) REFERENCES RFIDS(PageNumber,bid))');
 
 
 
