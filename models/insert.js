428621d9620e19be754248ab071be0c051a1d344
Mohamed Ayman <m.ayman854@gmail.com>
2016-04-22 13:46:33 +0200

    make it better

--- models/insert.js
+++ models/insert.js
@@ -5,19 +5,19 @@ var client = new pg.Client(connectionString);
 client.connect();
 
 
-var queryinsert= client.query("INSERT INTO BOOKS VALUES(default,'abc','efg',clock_timestamp(),clock_timestamp(),'Serawy','qwqw')");
+var queryinsert= client.query("INSERT INTO BOOKS VALUES(default,'9780495112402','7',clock_timestamp(),clock_timestamp(),'Serway','qwqw')");
 
-var queryinsert1= client.query("INSERT INTO RFIDS VALUES(default,1,'efg','1','abc',clock_timestamp(),clock_timestamp(),'asas')");
+var queryinsert1= client.query("INSERT INTO RFIDS VALUES(default,1,'7','19.5','9780495112402',clock_timestamp(),clock_timestamp(),'asas')");
 
-var queryinsert2= client.query("INSERT INTO admin_answers VALUES(default,'abc',1,'1','efg',clock_timestamp(),clock_timestamp(),'Hello dido?','please','1','qwqw')");
+var queryinsert2= client.query("INSERT INTO admin_answers VALUES(default,'9780495112402',1,'19.5','7',clock_timestamp(),clock_timestamp(),'Hello dido?','please','1','qwqw')");
 
-var queryinsert3= client.query("INSERT INTO QUESTIONS VALUES(default,1,'abc','1','efg',clock_timestamp(),clock_timestamp(),'hello?',5,'Alan','2.5','qwqw')");
+var queryinsert3= client.query("INSERT INTO QUESTIONS VALUES(default,1,'9780495112402','19.5','7',clock_timestamp(),clock_timestamp(),'hello?',5,'Alan','2.5','qwqw')");
 
-var queryinsert4= client.query("INSERT INTO ANSWERS VALUES(default,1,1,'abc','1','efg',clock_timestamp(),clock_timestamp(),'yea',5,'Alan','2.5','qwqw')");
+var queryinsert4= client.query("INSERT INTO ANSWERS VALUES(default,1,1,'9780495112402','19.5','7',clock_timestamp(),clock_timestamp(),'yea',5,'Alan','2.5','qwqw')");
 
-var queryinsert5= client.query("INSERT INTO LINKS VALUES(default,1,'abc','1','efg',clock_timestamp(),clock_timestamp(),'www.google.com',5,'Alan','2.5','wqw')");
+var queryinsert5= client.query("INSERT INTO LINKS VALUES(default,1,'9780495112402','19.5','7',clock_timestamp(),clock_timestamp(),'www.google.com',5,'Alan','2.5','wqw')");
 
-var queryinsert6= client.query("INSERT INTO SUMMARIES VALUES(default,1,'abc','1','efg',clock_timestamp(),clock_timestamp(),'Doing a good job',5,'Alan','2.5','wqw')");
+var queryinsert6= client.query("INSERT INTO SUMMARIES VALUES(default,1,'9780495112402','19.5','7',clock_timestamp(),clock_timestamp(),'Doing a good job',5,'Alan','2.5','wqw')");
 
 var queryinsert7= client.query("INSERT INTO USERS VALUES(default,'Alan Harber','12345','Alan@gmail.com')");
 
